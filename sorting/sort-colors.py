8db328f777ccce412908b8c0e26306771a23abb7
Meenakshi Iyer <47307666+ammuiyer@users.noreply.github.com>
2024-03-29 23:05:59 -0400

    :ballot_box_with_check: sort-colors [Runtime: 31 ms; Memory: 16.6 MB]

--- sorting/sort-colors.py
+++ sorting/sort-colors.py
@@ -3,18 +3,17 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        count0, count1, count2 = 0, 0, 0
-        for x in nums:
-            if x == 0:
-                count0+=1
-            if x == 1:
-                count1+=1
-            if x == 2:
-                count2+=1
-        for i in range(0, len(nums)):
-            if i<count0:
-                nums[i] = 0
-            elif i<count0+count1:
-                nums[i] = 1
-            elif i<count0+count1+count2:
-                nums[i] = 2
+        left, mid = 0, 0
+        right = len(nums)-1
+        while mid <= right:
+            if nums[mid]==0:
+                #swap w left
+                nums[mid], nums[left] = nums[left], nums[mid]
+                left+=1
+            if nums[mid]==2:
+                nums[mid], nums[right] = nums[right], nums[mid]
+                right-=1
+            else:
+                mid+=1
+
+            
