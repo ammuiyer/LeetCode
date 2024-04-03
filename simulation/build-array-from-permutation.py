03e0f89406530c4fcded2c27bdc1fc3918117341
Meenakshi Iyer <47307666+ammuiyer@users.noreply.github.com>
2024-04-02 21:44:59 -0400

    :ballot_box_with_check: build-array-from-permutation [Runtime: 88 ms; Memory: 16.7 MB]

--- simulation/build-array-from-permutation.py
+++ simulation/build-array-from-permutation.py
@@ -1,7 +1,4 @@
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
-        ans = []
-        for i in range(0, len(nums)):
-            ans.append(nums[nums[i]])
-        return ans
+        return [nums[nums[i]] for i in range(len(nums))]
         
