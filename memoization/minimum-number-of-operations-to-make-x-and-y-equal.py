051ccc764c652cc15c3c49ff290e72d08e99c68f
Meenakshi Iyer <47307666+ammuiyer@users.noreply.github.com>
2024-05-13 23:28:07 -0400

    :ballot_box_with_check: minimum-number-of-operations-to-make-x-and-y-equal [Runtime: 88 ms; Memory: 17.7 MB]

--- memoization/minimum-number-of-operations-to-make-x-and-y-equal.py
+++ memoization/minimum-number-of-operations-to-make-x-and-y-equal.py
@@ -1,22 +1,27 @@
 class Solution:
     def minimumOperationsToMakeEqual(self, x: int, y: int) -> int:
         #do bfs w a q
-        q = [(x, 0)]
+        q = [x]
+        layer = 0
         visited = set()
-        while True:
+        while q:
             #do all ops on a thing 
-            curr, layer = q.pop(0)
-            if curr == y:
-                return layer
-            if curr%11==0 and curr/11 not in visited:
-                q.append((curr/11, layer+1))
-            if curr%5==0 and curr/5 not in visited:
-                q.append((curr/5, layer+1))
-            if curr-1 not in visited:
-                q.append((curr-1, layer+1))
-            if curr+1 not in visited:
-                q.append((curr+1, layer+1))
-            visited.add(curr)
+            currLen = len(q)
+            for i in range(currLen):
+                curr = q.pop(0)
+                if curr == y:
+                    return layer
+                if curr%11==0 and curr/11 not in visited:
+                    q.append(curr/11)
+                if curr%5==0 and curr/5 not in visited:
+                    q.append(curr/5)
+                if curr>1 and curr-1 not in visited:
+                    q.append(curr-1)
+                if curr+1 not in visited:
+                    q.append(curr+1)
+                visited.add(curr)
+            layer+=1
+        
         
             
             
