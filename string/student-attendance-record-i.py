d51d5e568e51880901dc4a9d0a7c19a721937da8
Meenakshi Iyer <47307666+ammuiyer@users.noreply.github.com>
2024-05-26 12:12:16 -0500

    :ballot_box_with_check: student-attendance-record-i [Runtime: 31 ms; Memory: 16.7 MB]

--- string/student-attendance-record-i.py
+++ string/student-attendance-record-i.py
@@ -1,18 +1,5 @@
 class Solution:
     def checkRecord(self, s: str) -> bool:
-        countL = 0
-        countA = 0
-        for x in s:
-            if x=="L":
-                countL+=1
-                if countL>2:
-                    return False
-            else:
-                countL=0
-            if x=="A":
-                countA+=1
-                if countA>=2:
-                    return False
-            
-        return True
+        return (s.count("A") < 2) and ("LLL" not in s)
+
         
