7948b84f391f3e66f1884cd9a21a8a7fed1442d8
Meenakshi Iyer <47307666+ammuiyer@users.noreply.github.com>
2024-07-11 19:22:06 -0500

    :ballot_box_with_check: daily-temperatures [Runtime: 945 ms; Memory: 30.5 MB]

--- monotonic-stack/daily-temperatures.py
+++ monotonic-stack/daily-temperatures.py
@@ -5,7 +5,7 @@ class Solution:
         for i in range(1, len(temperatures)):
             while len(stack)>0 and temperatures[i]>temperatures[stack[len(stack)-1]]:
                 ret[stack[len(stack)-1]] = i-stack[len(stack)-1]
-                stack = stack[:-1]
+                del stack[-1]
             stack.append(i)
         return ret
         
