d5e28ead436b6e941d42253ae4ebe81abfc022b2
Meenakshi Iyer <47307666+ammuiyer@users.noreply.github.com>
2024-09-05 18:02:05 -0500

    LC-find-missing-observations [Runtime: N/A; Memory: N/A]

---
+++ math/find-missing-observations.py
@@ -0,0 +1,16 @@
+class Solution:
+    def missingRolls(self, rolls: List[int], mean: int, n: int) -> List[int]:
+        ret = []
+        currsum = sum(rolls)
+        goal = (mean*(len(rolls)+n)-currsum)
+        if goal/6 > n:
+            return []
+        else:
+            a = goal//n
+            rem = goal - a*n
+            # print(goal, a, rem)
+            for i in range(n):
+                ret.append(a)
+            ret[0]+=rem
+        return ret
+        
