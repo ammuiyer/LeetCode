fe11925cfc25490573b81caaf758572473638e98
Meenakshi Iyer <47307666+ammuiyer@users.noreply.github.com>
2024-09-24 16:04:33 -0500

    LC-find-the-length-of-the-longest-common-prefix [Runtime: N/A; Memory: N/A]

--- hash-table/find-the-length-of-the-longest-common-prefix.py
+++ hash-table/find-the-length-of-the-longest-common-prefix.py
@@ -1,8 +1,8 @@
 class Solution:
     def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
         ma = 0
-        for i in arr1:
-            for j in arr2:
+        for i in list(set(arr1)):
+            for j in list(set(arr2)):
                 a = str(i)
                 b = str(j)
                 count = 0
