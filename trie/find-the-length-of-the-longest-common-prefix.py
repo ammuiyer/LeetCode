b3f49f17a8126365c9c49dc3124a9283144cc88a
Meenakshi Iyer <47307666+ammuiyer@users.noreply.github.com>
2024-09-24 16:18:30 -0500

    LC-find-the-length-of-the-longest-common-prefix [Runtime: N/A; Memory: N/A]

--- trie/find-the-length-of-the-longest-common-prefix.py
+++ trie/find-the-length-of-the-longest-common-prefix.py
@@ -2,13 +2,21 @@ class Solution:
     def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
         ma = 0
         d = {}
-        d["0"] = []
-        d["0"] = []
-        d["0"] = []
+        d["1"] = []
+        d["2"] = []
+        d["3"] = []
+        d["4"] = []
+        d["5"] = []
+        d["6"] = []
+        d["7"] = []
+        d["8"] = []
+        d["9"] = []
         for i in list(set(arr1)):
-            for j in list(set(arr2)):
-                a = str(i)
-                d[a[0]].append(a)
+            a = str(i)
+            d[a[0]].append(a)
+        for i in list(set(arr2)):
+            a = str(i)
+            for j in d[a[0]]:
                 b = str(j)
                 count = 0
                 for x,y in zip(a,b):
